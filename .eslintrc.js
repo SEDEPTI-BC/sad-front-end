312848cb73ef0dc0c069932ece560b37fe0edc2c
quaresmateo <teoian.giovane@gmail.com>
2020-02-07 13:57:09 -0300

    Corrige erro da estensão Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,7 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    "@vue/standard",
-    "plugin:prettier/recommended",
-    "plugin:vue/essential"
-  ],
+  extends: ["@vue/standard", "plugin:vue/essential"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
