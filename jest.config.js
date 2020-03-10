1d3f548e1b7575d777888455b02172ef1fe38dc6
quaresmateo <teoian.giovane@gmail.com>
2020-03-10 12:37:53 -0300

    Adiciona novo template de admin usando Vuetify

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,9 @@ module.exports = {
     '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
     '^.+\\.jsx?$': 'babel-jest',
   },
+  transformIgnorePatterns: [
+    '/node_modules/',
+  ],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
@@ -17,7 +20,11 @@ module.exports = {
     'jest-serializer-vue',
   ],
   testMatch: [
-    '**/unit/**/*.spec.(js|jsx|ts|tsx)',
+    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)',
   ],
   testURL: 'http://localhost/',
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
 }
