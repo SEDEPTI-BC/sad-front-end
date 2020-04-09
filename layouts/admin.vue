61a503bc00b065c076df393a73d0a73ca13ffba0
quaresmateo <teoian.giovane@gmail.com>
2020-04-09 14:28:37 -0300

    Cria layout para admin

---
+++ layouts/admin.vue
@@ -0,0 +1,14 @@
+<template>
+  <NavBar />
+</template>
+
+<script>
+import NavBar from '~/components/NavBar'
+export default {
+  name: 'admin',
+  components: { NavBar }
+}
+</script>
+
+<style>
+</style>
