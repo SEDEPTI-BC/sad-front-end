0b4ae5a030a9f1c16414a0851e70ea20df752ac9
quaresmateo <teoian.giovane@gmail.com>
2020-04-16 15:45:40 -0300

    Cria páginas para usuário público

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -5,6 +5,9 @@ import scrollBehavior from './router.scrollBehavior.js'
 
 const _ad8531ae = () => interopDefault(import('../pages/admin/calendario.vue' /* webpackChunkName: "pages/admin/calendario" */))
 const _166578b8 = () => interopDefault(import('../pages/admin/equipamentos.vue' /* webpackChunkName: "pages/admin/equipamentos" */))
+const _72d66182 = () => interopDefault(import('../pages/public/agendamentos.vue' /* webpackChunkName: "pages/public/agendamentos" */))
+const _e16594d0 = () => interopDefault(import('../pages/public/sobre.vue' /* webpackChunkName: "pages/public/sobre" */))
+const _1bcbad2f = () => interopDefault(import('../pages/public/login/entrar.vue' /* webpackChunkName: "pages/public/login/entrar" */))
 const _23d2359d = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 // TODO: remove in Nuxt 3
@@ -31,6 +34,18 @@ export const routerOptions = {
     path: "/admin/equipamentos",
     component: _166578b8,
     name: "admin-equipamentos"
+  }, {
+    path: "/public/agendamentos",
+    component: _72d66182,
+    name: "public-agendamentos"
+  }, {
+    path: "/public/sobre",
+    component: _e16594d0,
+    name: "public-sobre"
+  }, {
+    path: "/public/login/entrar",
+    component: _1bcbad2f,
+    name: "public-login-entrar"
   }, {
     path: "/",
     component: _23d2359d,
