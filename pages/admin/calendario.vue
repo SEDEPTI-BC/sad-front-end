7e31b995751fa4db62adcb537d5fff91a560c34d
quaresmateo <teoian.giovane@gmail.com>
2020-05-03 12:11:11 -0300

    Cria página de calendário

--- pages/admin/calendario.vue
+++ pages/admin/calendario.vue
@@ -1,13 +1,284 @@
 <template>
-  <div>
-    <h1>Calendário</h1>
-  </div>
+  <section class="container">
+    <header>
+      <h1>Gerenciar datas e horários</h1>
+      <button class="btn btn-secondary">
+        <b-icon-calendar />
+        <span>Desativar dia</span>
+      </button>
+    </header>
+    <div class="body">
+      <div class="calendar mb-5">
+        <header class="calendar-header">
+          <strong>Calendário</strong>
+        </header>
+        <div class="calendar-body">
+          <b-calendar
+            v-model="value"
+            block
+            :date-disabled-fn="dateDisabled"
+            hide-header
+            locale="pt-BR"
+            :weekdays="weekdays"
+            today-variant="danger"
+            selected-variant="danger"
+            v-bind="labels"
+            @context="onContext"
+          ></b-calendar>
+        </div>
+      </div>
+      <div class="card-list">
+        <header class="card-list-header">
+          <strong>Dias desativados</strong>
+        </header>
+        <div class="card-list-body">
+          <div
+            v-for="day in disableDays"
+            :key="day.id"
+            class="card-list-content"
+          >
+            <header>
+              <strong>{{ day.title | capitalize }}</strong>
+              <div>
+                <button class="btn">
+                  <b-icon-trash class="trash" />
+                </button>
+                <button class="btn">
+                  <b-icon-pencil-square class="edit" />
+                </button>
+              </div>
+            </header>
+            <p>{{ day.description }}</p>
+            <div style="font-size:0.8rem;">
+              <div><b>De</b>: {{ day.start }}</div>
+              <div><b>Até</b>: {{ day.end }}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import { BIconCalendar, BIconPencilSquare, BIconTrash } from 'bootstrap-vue'
 export default {
-  layout: 'admin'
+  layout: 'admin',
+  name: 'Calendario',
+  components: {
+    BIconCalendar,
+    BIconPencilSquare,
+    BIconTrash
+  },
+  data() {
+    return {
+      disableDays: [
+        {
+          id: 1,
+          user_id: 1,
+          start: '2020-05-10 08:00:00',
+          end: '2020-05-11 00:00:00',
+          title: 'dia das mães',
+          description:
+            'Dia das Mães ou Dia da Mãe é uma data comemorativa que homenageia anualmente a figura familiar materna e a maternidade. A data de comemoração varia de acordo com o país.',
+          created_at: '2020-04-15 12:09:59',
+          updated_at: '2020-04-15 12:09:59'
+        },
+        {
+          id: 2,
+          user_id: 5,
+          start: '2020-06-11 00:00:00',
+          end: '2020-06-12 00:00:00',
+          title: 'corpus christi',
+          description:
+            'Corpus Christi, ou Corpus Domini e generalizada em Portugal como Corpo de Deus, é uma comemoração litúrgica católica que ocorre na quinta-feira seguinte ao domingo da Santíssima Trindade, que, por sua vez, acontece no domingo seguinte ao de Pentecostes.',
+          created_at: '2020-04-15 12:09:59',
+          updated_at: '2020-04-15 12:09:59'
+        },
+        {
+          id: 3,
+          user_id: 3,
+          start: '2020-06-12 00:00:00',
+          end: '2020-06-13 00:00:00',
+          title: 'Dia dos namorados',
+          description: '',
+          created_at: '2020-04-15 12:09:59',
+          updated_at: '2020-04-15 12:09:59'
+        },
+        {
+          id: 4,
+          user_id: 2,
+          start: '2020-04-19 05:00:00',
+          end: '2020-04-19 09:00:00',
+          title: 'Dia dos pais',
+          description:
+            'Dia dos Pais ou Dia do Pai é uma data comemorativa que homenageia anualmente a figura familiar paterna. A data varia de acordo com os países.',
+          created_at: '2020-04-15 12:09:59',
+          updated_at: '2020-04-15 12:09:59'
+        },
+        {
+          id: 5,
+          user_id: 4,
+          start: '2020-09-05 08:00:00',
+          end: '2020-09-05 12:00:00',
+          title: 'Desratização',
+          description: '',
+          created_at: '2020-04-15 12:09:59',
+          updated_at: '2020-04-15 12:09:59'
+        }
+      ],
+      value: '',
+      context: null,
+      labels: {
+        weekdayHeaderFormat: 'long',
+        labelPrevDecade: 'Década anterior',
+        labelPrevYear: 'Ano anterior',
+        labelPrevMonth: 'Mês anterior',
+        labelCurrentMonth: 'Mês atual',
+        labelNextMonth: 'Mês que vem',
+        labelNextYear: 'Mês que vem',
+        labelNextDecade: 'Próxima década',
+        labelToday: 'Hoje',
+        labelSelected: 'Data selecionada',
+        labelNoDateSelected: 'Nenhuma data selecionada',
+        labelCalendar: 'Calendário',
+        labelNav: 'Navegação do calendário',
+        labelHelp: 'Use as teclas de seta para navegar pelo calendário'
+      },
+      weekdays: [
+        { value: 0, text: 'Domingo' },
+        { value: 1, text: 'Segunda-feira' },
+        { value: 2, text: 'Terça-feira' },
+        { value: 3, text: 'Quarta-feira' },
+        { value: 4, text: 'Quinta-feira' },
+        { value: 5, text: 'Sexta-feira' },
+        { value: 6, text: 'Sábado' }
+      ]
+    }
+  },
+  methods: {
+    dateDisabled(ymd, date) {
+      const weekday = date.getDay()
+      const day = date.getDate()
+      return weekday === 0 || weekday === 6 || day === 13
+    },
+    onContext(ctx) {
+      this.context = ctx
+    }
+  }
 }
 </script>
 
-<style></style>
+<style scoped>
+header {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 40px 0;
+  justify-content: space-between;
+}
+
+span {
+  margin-left: 5px;
+}
+
+.body {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+}
+
+.btn {
+  float: right;
+}
+
+.calendar {
+  height: 350px;
+  max-width: 400px;
+  /* margin-bottom: 40px;  */
+  flex-grow: 2;
+}
+
+.calendar-body {
+  background: #ddd;
+  border-radius: 0 0 4px 4px;
+  height: inherit;
+  margin: 0;
+  padding: 5px;
+}
+
+.calendar-header {
+  background: #6c757d;
+  border-radius: 4px 4px 0 0;
+  margin: 0;
+  padding: 10px 5px;
+}
+
+.calendar-header strong {
+  margin: 0 auto;
+  color: #eee;
+}
+
+.card-list {
+  height: 350px;
+  max-width: 400px;
+  margin-bottom: 40px;
+}
+
+.card-list-body {
+  background: #ddd;
+  border-radius: 0 0 4px 4px;
+  height: inherit;
+  margin: 0;
+  overflow-y: scroll;
+  padding: 0;
+}
+
+.card-list-content {
+  background: #eee;
+  border-radius: 4px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  width: calc(100% - 20px);
+  margin: 10px 0 10px 10px;
+  padding: 10px 15px;
+}
+
+.card-list-content div {
+  margin-top: 0px;
+}
+
+.card-list-content header {
+  align-items: center;
+  margin: 0;
+}
+
+.card-list-content header .btn {
+  margin: 0 0 0 10px;
+  padding: 0;
+}
+
+.card-list-content header strong {
+  font-size: 1.3rem;
+}
+
+.card-list-content p {
+  padding: 20px 0;
+}
+
+.card-list-header {
+  background: #6c757d;
+  border-radius: 4px 4px 0 0;
+  margin: 0;
+  padding: 10px 5px;
+}
+
+.card-list-header strong {
+  margin: 0 auto;
+  color: #eee;
+}
+
+.container {
+  display: grid;
+  margin: 0 auto;
+  max-width: 900px;
+}
+</style>
