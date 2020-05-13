f549671663c2b2d68d4f697edd3520424a284f0e
quaresmateo <teoian.giovane@gmail.com>
2020-05-13 18:02:10 -0300

    Cria auth

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,6 +2,11 @@ export default function({ $axios }, inject) {
   const api = $axios.create({
     baseURL: process.env.baseUrl
   })
+  const token = localStorage.getItem('user-token')
+  if (token) {
+    // eslint-disable-next-line dot-notation
+    api.defaults.headers.common['Authorization'] = token
+  }
 
   // Inject to context as $api
   inject('api', api)
