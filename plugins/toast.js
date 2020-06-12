88450951eb44e2a8f6e8c9db9accde1a8139f47c
quaresmateo <teoian.giovane@gmail.com>
2020-06-12 15:33:58 -0300

    Adiciona opção de não esconder toast

--- plugins/toast.js
+++ plugins/toast.js
@@ -1,7 +1,13 @@
-export function makeToast(message, variant = null, title = 'SAD-BC') {
+export function makeToast(
+  message,
+  variant = null,
+  noAutoHide = false,
+  title = 'SAD-BC'
+) {
   this.$bvToast.toast(message, {
     title,
     variant,
+    noAutoHide,
     solid: true
   })
 }
